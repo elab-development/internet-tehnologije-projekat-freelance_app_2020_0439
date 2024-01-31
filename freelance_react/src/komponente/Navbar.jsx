cd78893a9d595b97e8ceed9fdab49e40dd863f15
Andjela Milićević <amilicevic574@gmail.com>
2024-01-31 22:18:48 +0100

    Intalirane biblioteke react-router-dom and react-icons, i implementiran navigacioni meni

---
+++ freelance_react/src/komponente/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="nav-links">
+                <li>
+                <img className='logo'  src="https://i.ibb.co/QrSXky7/image-Photo-Room-png-Photo-Room.png" alt="logo" border="0"/>
+                </li>
+                <li>
+                    <Link to="/">Početna</Link>
+                </li>
+                <li>
+                    <Link to="/usluge">Usluge</Link>
+                </li>
+                <li>
+                    <Link to="/o-nama">O nama</Link>
+                </li>
+                <li>
+                    <Link to="/musterije">Nase musterije</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
